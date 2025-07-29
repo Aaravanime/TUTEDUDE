2a910fb796596297f58ffa4d72b9451742841cd4
Aaravanime <148857892+Aaravanime@users.noreply.github.com>
2025-07-29 21:24:24 +0530

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,12 @@
+a=input("Enter the first number")
+b=input("Enter the second  number")
+a=int(a)
+b=int(b)
+c=a + b
+print(c)
+d = a - b
+print(d)
+e= a * b
+print(e)
+f=a/b
+print(f)
---
+++ task 2.py
@@ -0,0 +1,4 @@
+a=input("your first name ")
+b=input("your last name ")
+c=a + " " + b
+print(f"hello,{c}! welocome to this program ")
